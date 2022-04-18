781e6fe364002d084547a1216b7e227b3b68f94b
min-hinthar <102896221+min-hinthar@users.noreply.github.com>
2022-04-18 04:09:01 -0700

    4 a.m commit

--- javascript.js
+++ javascript.js
@@ -1,1 +1,16 @@
+const box = document.getElementById('box');
 
+function grow() {
+    box.setAttribute.("id", "growBox");
+}
+function blue() {
+    box.setAttribute.("id", "blueBox");
+}
+function fade() {
+    box.setAttribute("id", "fadeBox");
+    setTimeout(funcion());
+    }
+}
+function reset() {
+    box.setAttribute("id", "resetBox")
+}
